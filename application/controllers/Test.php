bb0e6ebbc5c870eecf116b2676a3b0cb907575de
seedsat <84009566+seedsat@users.noreply.github.com>
2022-01-19 19:55:43 +0900

    test commit-2

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -3,4 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends MY_Controller {
 
+    public function __construct()
+  {
+      parent::__construct();
+  }
+
 }
